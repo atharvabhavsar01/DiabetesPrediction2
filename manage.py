0f81cb2438ebfabed01290da9177bb4528353a69
atharvabhavsar01 <atharvabhavsar01@gmail.com>
2023-05-10 09:16:39 +0530

    qqq

--- manage.py
+++ manage.py
@@ -3,11 +3,13 @@
 import os
 import sys
 
+print("hello")
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiabetesPrediction.settings")
+     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiabetesPrediction.settings")
     try:
+        
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
